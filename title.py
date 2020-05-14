a623d3e776699cd3398345d8b26cbf14b3d7317f
aadkin13 <49595671+aadkin13@users.noreply.github.com>
2020-05-14 11:36:50 -0400

    Update title.py

--- title.py
+++ title.py
@@ -1,6 +1,5 @@
-# This code if or the Knowledge Center
+# This code if or the Knowledge Center title screen
 import pygame
-import time
 
 pygame.init()
 
@@ -10,35 +9,44 @@ green = [0, 255, 0]
 blue = [0, 0, 255]
 red = [255, 0, 0]
 
-# screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-screen = pygame.display.set_mode((500, 500))
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
+# screen = pygame.display.set_mode((500, 500))
 width = pygame.display.Info().current_w
 height = pygame.display.Info().current_h
 
 screen.fill(black)
 pygame.display.update()
 
-font = pygame.font.Font('freesansbold.ttf', 64)
+font = pygame.font.Font('freesansbold.ttf', 58)
 
 text = font.render('Hello, Welcome to the Discovery Place', True, green, black)
 text2 = font.render('This is the Knowledge Center', True, green, black)
+text3 = font.render('Please place your artifact here', True, green, black)
 
 textRect = text.get_rect()
 text2Rect = text2.get_rect()
+text3Rect = text3.get_rect()
 
-textRect.center = (width / 2, height / 2)
-text2Rect.center = (width/2 + 90, height/2 + 90)
+textRect.center = (width / 2, height / 2 - 300)
+text2Rect.center = (width / 2, height / 2 - 150)
+text3Rect.center = (width / 2 + 90, height / 2)
+
+arrow = pygame.image.load('arrow.jpg')
 
 run = True
 while run:
     screen.blit(text, textRect)
     screen.blit(text2, text2Rect)
-    
+    screen.blit(arrow, (width / 2 - 600, height / 2 - 90))
+    screen.blit(text3, text3Rect)
+
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 run = False
-                
+
     pygame.display.update()
-                
+
 pygame.display.quit()
+quit()
